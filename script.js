a0117b39f5256a5ce7b07dc3cf9ad01b3f677cce
Parker Miller <parkermiller00@gmail.com>
2022-09-27 22:30:25 -0600

    Add info box animations

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ const grapeRef = document.querySelector('.grape-container')
 const sceneRef = document.querySelector('.scene-container')
 const infoRef = document.querySelector('.info-container')
 
+let currentInfoRef = null;
+
 function addGrape({
   top, left, info
 }) {
   const newGrape = grapeRef.cloneNode(true)
-
   newGrape.style.top = top
   newGrape.style.left = left
   newGrape.hidden = false
 
   newGrape.onclick = (e) => {
-    infoRef.querySelector('.info').innerHTML = info
-    infoRef.style.left = e.pageX+'px'
-    infoRef.style.top = e.pageY+'px'
-    infoRef.hidden = false
+    const newInfo = infoRef.cloneNode(true)
+    newInfo.querySelector('.info').innerHTML = info
+    newInfo.style.left = `calc(${newGrape.style.left} + 32px)`
+    newInfo.style.top = `calc(${newGrape.style.top} + 32px)`
+    newInfo.hidden = false
+
+    // Delete the old info
+    if (currentInfoRef) { currentInfoRef.remove() }
+    currentInfoRef = newInfo
+
+    sceneRef.appendChild(newInfo)
+    requestAnimationFrame(() => {
+      // Update styles to trigger animation transition
+      newInfo.style.opacity = '100%'
+      newInfo.style.transform = 'scale(1)'
+    })
   }
 
   sceneRef.appendChild(newGrape)
